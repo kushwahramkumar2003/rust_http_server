ad6bd7fc204513634d1a64a51ae877e4853c6524
Himanshu Gupta <himanshu@trytemelio.com>
2025-11-22 22:06:31 +0530

    Refactor HTTP server to implement a thread pool for handling connections, improving concurrency and job management.

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,97 @@
-use std::{ io::{IoSlice, Read, Write}, iter::StepBy, net::TcpListener, sync::mpsc, thread::{self, sleep}, time::Duration};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+    sync::{mpsc, Arc, Mutex},
+    thread::{self, sleep},
+    time::Duration,
+};
 
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
 
-// struct Worker{
-//     id:usize,
-//     thread: thread::JoinHandle<()>
-// }
-// pub struct ThreadPool {
-//     threads: Vec<Worker>,
-//     sender: mpsc::Sender<Job>,
-// }
-
-// impl Worker{
-//     pub fn new(id:usize,receiver: mpsc::Receiver<Job>) -> Worker {
-//         Worker {
-//             id,
-//             thread: thread::spawn(move ||{
-//                 receiver;
-//             })
-//         }
-//     }
-// }
-
-// impl ThreadPool {
-//     pub fn new(size: usize) -> ThreadPool {
-//         assert!(size > 0);
-
-//         let mut threads = Vec::with_capacity(size);
-
-//         for id in 0..size {
-//             let worker = Worker::new(id);
-//             threads.push(worker);
-//         }
-
-//         ThreadPool {
-//             threads
-//         }
-//     }
-// }
-fn main() {
-    let listner = TcpListener::bind("127.0.0.1:8080").unwrap();
-
-    for mut stream in listner.incoming(){
-
-        thread::spawn(move ||{
-        match stream {
-            Ok(mut stream)=>{
-
-                let mut buffer = [0; 1024];
-                stream.read(&mut buffer).unwrap();
-
-                String::from_utf8_lossy(&buffer);
-                println!("Request : {:?}",String::from_utf8_lossy(&buffer[..]));
-
-                println!("Some : {:?}",stream);
-                let msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!";
-                
-                stream.write_all(msg.as_bytes()).unwrap();
-                sleep(Duration::from_secs(10));
-
-                stream.write_timeout().unwrap();
-                
-                stream.flush().unwrap()
-                
-            }
-            Err(e)=> println!("Error : {:?}",e)
-        }
-        }
-    );
-       
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
     }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} got a job; executing.", id);
+                job.call_box();
+            }
+        });
+
+        Worker { id, thread }
+    }
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4); // 4 threads
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                pool.execute(|| {
+                    handle_connection(stream);
+                });
+            }
+            Err(e) => println!("Error: {:?}", e),
+        }
+    }
+}
+
+fn handle_connection(mut stream: std::net::TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    // println!("Request: {:?}", String::from_utf8_lossy(&buffer[..]));
+
+    let msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!";
+
+    stream.write_all(msg.as_bytes()).unwrap();
+    sleep(Duration::from_secs(100));
+
+    stream.flush().unwrap();
 }
